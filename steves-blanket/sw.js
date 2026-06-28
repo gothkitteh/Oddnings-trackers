de911fbb6f97a2dafab93a9a88cdd66f132de259
Shellie M <54857346+gothkitteh@users.noreply.github.com>
2026-06-27 17:35:26 -0700

    Implement service worker for caching assets

--- steves-blanket/sw.js
+++ steves-blanket/sw.js
@@ -1,1 +1,29 @@
+const CACHE = 'steves-blanket-v1';
+const ASSETS = [
+  '/Oddnings-trackers/steves-blanket/',
+  '/Oddnings-trackers/steves-blanket/index.html',
+  '/Oddnings-trackers/steves-blanket/manifest.json',
+  '/Oddnings-trackers/steves-blanket/icon-192.png',
+];
 
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS).catch(() => {}))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
