5a09b73f8bcc60ee234590661908ca11d313954a
Shellie M <shellie.meeks.shaffner@gmail.com>
2026-07-07 15:07:07 -0700

    Initial Commit
    Add Pando Aspen crochet tracker

---
+++ pando-aspen/sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'pandoaspen-v1';
+const ASSETS = [
+  '/Oddnings-trackers/pando-aspen/',
+  '/Oddnings-trackers/pando-aspen/index.html',
+  '/Oddnings-trackers/pando-aspen/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
